e5a292286949c9b80b175311a68183922fdde261
Stoyan Lilov <stoyan.lilov@egt-bg.com>
2016-10-18 12:33:49 +0300

    Bingo Page

--- resources/views/settings/bingo.blade.php
+++ resources/views/settings/bingo.blade.php
@@ -10,9 +10,9 @@
 <!-- Bingo Main Conf Panel -->
 <div class="row">
 	<div class="col-lg-12">
-		<div class="panel panel-default">
+		<div class="panel panel-primary">
 
-		  <div class="panel-heading">
+		  <div  class="panel-heading">
 		    <h3 class="panel-title text-center">Main Configurations</h3>
 		  </div>
 
@@ -41,16 +41,16 @@
 					</div>
 
 					<div class="form-group">
-					    	<label style="color: #474747">Cost Line 2:</label><br>
-							<div class="input-group">
+					    <label style="color: #474747">Cost Line 2:</label><br>
+						<div class="input-group">
 								<span class="input-group-addon" id="sizing-addon2">$</span>
 								<input value="{{ $bingo->bingo_cost_line2 }}" type="text" class="form-control" placeholder="Ticket Cost" aria-describedby="sizing-addon2">
 						</div>
 					</div>
 
 					<div class="form-group">
-					    	<label style="color: #474747">Cost Bingo:</label><br>
-							<div class="input-group">
+					    <label style="color: #474747">Cost Bingo:</label><br>
+						<div class="input-group">
 								<span class="input-group-addon" id="sizing-addon2">$</span>
 								<input value="{{ $bingo->bingo_cost_bingo }}" type="text" class="form-control" placeholder="Ticket Cost" aria-describedby="sizing-addon2">
 						</div>
@@ -173,8 +173,91 @@
 
 		  </div><!-- End Panel Body -->
 
-		  <div class="panel-footer"><button class="btn btn-danger text-center">Edit</button></div>
+		<div class="panel-footer">
+			<!-- Rounded switch -->
+			<label class="switch">
+			  <input id="locker" type="checkbox" name="lock">
+			  <div class="slider round"></div>
+			</label>
+
+		</div>
 
 		</div><!-- End Panel -->
 	</div><!-- End Col -->
 </div><!-- End Row -->
+
+<script>
+	$('#locker').click(function() {
+	    $("#locker").toggle(this.checked);
+	});
+
+	$('input[type=checkbox]').change(function(){
+	    if(this.checked) {
+	        $('input').prop('disabled', true);
+	    } else {
+	        $('input').prop('disabled', false);
+	    }
+	});
+
+</script>
+
+<style>
+	/* The switch - the box around the slider */
+	.switch {
+	  position: relative;
+	  display: inline-block;
+	  width: 60px;
+	  height: 34px;
+	}
+
+	/* Hide default HTML checkbox */
+	.switch input {display:none;}
+
+	/* The slider */
+	.slider {
+	  position: absolute;
+	  cursor: pointer;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+	  background-color: #b30000;
+	  -webkit-transition: .4s;
+	  transition: .4s;
+	}
+
+	.slider:before {
+	  position: absolute;
+	  content: "";
+	  height: 26px;
+	  width: 26px;
+	  left: 4px;
+	  bottom: 4px;
+	  background-color: white;
+	  -webkit-transition: .4s;
+	  transition: .4s;
+	}
+
+	input:checked + .slider {
+	  background-color: #2196F3;
+	}
+
+	input:focus + .slider {
+	  box-shadow: 0 0 1px #2196F3;
+	}
+
+	input:checked + .slider:before {
+	  -webkit-transform: translateX(26px);
+	  -ms-transform: translateX(26px);
+	  transform: translateX(26px);
+	}
+
+	/* Rounded sliders */
+	.slider.round {
+	  border-radius: 34px;
+	}
+
+	.slider.round:before {
+	  border-radius: 50%;
+	}
+</style>
