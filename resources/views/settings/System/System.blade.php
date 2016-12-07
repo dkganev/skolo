26be745ad9a2f9f36a9abd7b04198ca75f2c5120
Dimitar Ganev <dimitar.ganev@egt-bg.com>
2016-12-07 09:43:48 +0200

    Players Cards

--- resources/views/settings/System/System.blade.php
+++ resources/views/settings/System/System.blade.php
@@ -3,9 +3,9 @@
         <div class="col-md-12" >
 
             <div class="panel panel-default" >
-                <div class="panel-heading" style="height: 1200px;">
+                <div class="panel-heading" style="height: 1000px;">
                     
-                        <iframe src="http://10.0.0.156:8020/phpsysinfo-3.2.6/index.php?disp=dynamic" style="width: 100%; height: 100%">
+                        <iframe src="http://localhost:8020/phpsysinfo-3.2.6/index.php?disp=dynamic" style="width: 100%; height: 100%">
                         </iframe>
                     
                 </div>
