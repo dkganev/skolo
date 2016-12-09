56b3c1f2effa178b86628726dadbdfb5aecef9fd
Dimitar Ganev <dimitar.ganev@egt-bg.com>
2016-12-09 13:08:07 +0200

    statistics terminals

--- public/js/socket.io/statisticTerminals.js
+++ public/js/socket.io/statisticTerminals.js
@@ -20,7 +20,10 @@ function socketConect() {
         socket.disconnect();
         sockStatus = 0;
         console.log ("Disconect");
-        $("#socketConect").css({'background-image': 'linear-gradient(to bottom,#f5f5f5 0%,#e8e8e8 100%)'});
+        $("#socketConect").attr('style', 'background: rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(76, 76, 76, 1) 0%, rgba(89, 89, 89, 1) 12%, rgba(102, 102, 102, 1) 25%, rgba(71, 71, 71, 1) 39%, rgba(44, 44, 44, 1) 46%, rgba(0, 0, 0, 1) 51%, rgba(44, 44, 44, 1) 55%, rgba(17, 17, 17, 1) 60%, rgba(43, 43, 43, 1) 76%, rgba(28, 28, 28, 1) 91%, rgba(19, 19, 19, 1) 100%) repeat scroll 0 0 !important;'); 
+        ////style.setProperty('background', '#fff', 'important');  //style.setProperty( 'height', '0px', 'important' )
+        //$("#socketConect").css({'background-image': 'linear-gradient(to bottom,#f5f5f5 0%,#e8e8e8 100%)'});
+        //background: rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(76, 76, 76, 1) 0%, rgba(89, 89, 89, 1) 12%, rgba(102, 102, 102, 1) 25%, rgba(71, 71, 71, 1) 39%, rgba(44, 44, 44, 1) 46%, rgba(0, 0, 0, 1) 51%, rgba(44, 44, 44, 1) 55%, rgba(17, 17, 17, 1) 60%, rgba(43, 43, 43, 1) 76%, rgba(28, 28, 28, 1) 91%, rgba(19, 19, 19, 1) 100%) repeat scroll 0 0 !important;
         $("#socketConectRealtime").text('Start Real Time');
     }
 }
@@ -39,11 +42,11 @@ socket.on('new message', function (data) {
     var obj = $.parseJSON(jsonObj);
     console.log (obj);
     if (obj.query == "UPDATE") {
-        TotalIn = parseInt($('#TotalIn').text());
-        TotalOut = parseInt($('#TotalOut').text());
-        TotalBet = parseInt($('#TotalBet').text());
-        TotalWin = parseInt($('#TotalWin').text());
-        TotalCredit = parseInt($('#TotalCredit').text());
+        TotalIn = parseInt($('#TotalIn').attr("data-total"));
+        TotalOut = parseInt($('#TotalOut').attr("data-total"));
+        TotalBet = parseInt($('#TotalBet').attr("data-total"));
+        TotalWin = parseInt($('#TotalWin').attr("data-total"));
+        TotalCredit = parseInt($('#TotalCredit').attr("data-total"));
         TotalInOld  = obj.dataOld.counter[11] + obj.dataOld.counter[23] + obj.dataOld.counter[121]; //parseInt($('#TotalIn' + obj.dataNew.psid).text());
         TotalOutOld = obj.dataOld.counter[2] + obj.dataOld.counter[3] + obj.dataOld.counter[22] + obj.dataOld.counter[24]; //parseInt($('#TotalOut' + obj.dataNew.psid).text());
         TotalBetOld = obj.dataOld.counter[0]; //parseInt($('#TotalBet' + obj.dataNew.psid).text());
@@ -56,47 +59,52 @@ socket.on('new message', function (data) {
         TotalCreditNew = obj.dataNew.counter[12]; //TotalInNew + TotalWinNew - TotalBetNew - TotalOutNew;
         
         if (TotalInOld != TotalInNew){
-            $('#TotalIn' + obj.dataNew.psid).text(TotalInNew);
+            $('#TotalIn' + obj.dataNew.psid).text((TotalInNew/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
             $('#TotalIn' + obj.dataNew.psid).css({'color': 'red'});
             clearTimeout(TotalInTimeOut[obj.dataNew.psid]);
             TotalInTimeOut[obj.dataNew.psid] = setTimeout(function(){ $('#TotalIn' + obj.dataNew.psid).css({'color': '#333'}); }, 20000);
             TotalIn1 = TotalIn - TotalInOld + TotalInNew ;
-            $('#TotalIn').text(TotalIn1);
+            $('#TotalIn').attr("data-total", TotalIn1);
+            $('#TotalIn').text((TotalIn1/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
             //$('#TotalIn').text(TotalInNew);
         }
         if (TotalOutOld != TotalOutNew){
-            $('#TotalOut' + obj.dataNew.psid).text(TotalOutNew);
+            $('#TotalOut' + obj.dataNew.psid).text((TotalOutNew/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
             $('#TotalOut' + obj.dataNew.psid).css({'color': 'red'});
             clearTimeout(TotalOutTimeOut[obj.dataNew.psid]);
             TotalOutTimeOut[obj.dataNew.psid] = setTimeout(function(){ $('#TotalOut' + obj.dataNew.psid).css({'color': '#333'}); }, 20000);
             TotalOut = TotalOut - TotalOutOld + TotalOutNew ;
-            $('#TotalOut').text(TotalOut);
+            $('#TotalOut').attr("data-total", TotalOut);
+            $('#TotalOut').text((TotalOut/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
         }
         if (TotalBetOld != TotalBetNew){
-            $('#TotalBet' + obj.dataNew.psid).text(TotalBetNew);
+            $('#TotalBet' + obj.dataNew.psid).text((TotalBetNew/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
             $('#TotalBet' + obj.dataNew.psid).css({'color': 'red'});
             clearTimeout(TotalBetTimeOut[obj.dataNew.psid]);
             TotalBetTimeOut[obj.dataNew.psid] = setTimeout(function(){ $('#TotalBet' + obj.dataNew.psid).css({'color': '#333'}); }, 20000);
             TotalBet = TotalBet - TotalBetOld + TotalBetNew ;
-            $('#TotalBet').text(TotalBet);
+            $('#TotalBet').attr("data-total", TotalBet);
+            $('#TotalBet').text((TotalBet/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
         }
         if (TotalWinOld != TotalWinNew){
-            $('#TotalWin' + obj.dataNew.psid).text(TotalWinNew);
+            $('#TotalWin' + obj.dataNew.psid).text((TotalWinNew/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
             $('#TotalWin' + obj.dataNew.psid).css({'color': 'red'});
             clearTimeout(TotalWinTimeOut[obj.dataNew.psid]);
             TotalWinTimeOut[obj.dataNew.psid] = setTimeout(function(){ $('#TotalWin' + obj.dataNew.psid).css({'color': '#333'}); }, 20000);
             TotalWin = TotalWin - TotalWinOld + TotalWinNew ;
-            $('#TotalWin').text(TotalWin);
+            $('#TotalWin').attr("data-total", TotalWin);
+            $('#TotalWin').text((TotalWin/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
         }
         if (TotalCreditOld != TotalCreditNew){
-            $('#TotalCredit' + obj.dataNew.psid).text(TotalCreditNew);
+            $('#TotalCredit' + obj.dataNew.psid).text((TotalCreditNew/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
             $('#TotalCredit' + obj.dataNew.psid).css({'color': 'red'});
             clearTimeout(TotalCreditTimeOut[obj.dataNew.psid]);
             TotalCreditTimeOut[obj.dataNew.psid] = setTimeout(function(){ $('#TotalCredit' + obj.dataNew.psid).css({'color': '#333'}); }, 20000);
             TotalCredit = TotalCredit - TotalCreditOld + TotalCreditNew ;
-            $('#TotalCredit').text(TotalCredit);
+            $('#TotalCredit').attr("data-total", TotalCredit);
+            $('#TotalCredit').text((TotalCredit/100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
         }
-        //$("#tr" + obj.dataNew.psid).css({'background-color': 'red'});
+        //$("#tr" + obj.dataNew.psid).css({'background-color': 'red'}); 
        
     } else if (obj.query == "DELETE") {
         //
--- resources/views/statistics/terminals.blade.php
+++ resources/views/statistics/terminals.blade.php
@@ -52,11 +52,11 @@
                         @endforeach
                         <tr class="danger" >
                             <td class="text-center" colspan="2"><strong style="color:black;">TOTAL</strong></td>
-                            <td class="text-center" id="TotalIn">{{ number_format($totals['totalIn']/100, 2) }}</td>
-                            <td class="text-center" id="TotalOut">{{ number_format($totals['totalOut']/100, 2) }}</td>
-                            <td class="text-center" id="TotalBet">{{ number_format($totals['totalBet']/100, 2) }}</td>
-                            <td class="text-center" id="TotalWin">{{ number_format($totals['totalWin']/100, 2) }}</td>
-                            <td class="text-center" id="TotalCredit">{{ number_format($totals['totalCredit']/100, 2) }}</td>
+                            <td class="text-center" id="TotalIn" data-total="{{$totals['totalIn']}}">{{ number_format($totals['totalIn']/100, 2) }}</td>
+                            <td class="text-center" id="TotalOut" data-total="{{$totals['totalOut']}}">{{ number_format($totals['totalOut']/100, 2) }}</td>
+                            <td class="text-center" id="TotalBet" data-total="{{$totals['totalBet']}}">{{ number_format($totals['totalBet']/100, 2) }}</td>
+                            <td class="text-center" id="TotalWin" data-total="{{$totals['totalWin']}}">{{ number_format($totals['totalWin']/100, 2) }}</td>
+                            <td class="text-center" id="TotalCredit" data-total="{{$totals['totalCredit']}}">{{ number_format($totals['totalCredit']/100, 2) }}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -73,13 +73,25 @@
         socket.connect();
         //sockStatus = 1;
         console.log ("Conect"); //socketConect  background-color: #f5f5f5;
-        $("#socketConect").css({'background-image': 'linear-gradient(to bottom,#5cb85c 0%,#419641 100%)'});
+        //$("#socketConect").attr('style', 'background-image: linear-gradient(to bottom,#5cb85c 0%,#419641 100%) !important');
+        //
+        //background: rgba(164,179,87,1);
+        //background: -moz-linear-gradient(top, rgba(164,179,87,1) 0%, rgba(164,179,86,1) 57%, rgba(164,179,86,1) 62%, rgba(164,179,86,1) 72%, rgba(117,137,12,1) 100%);
+        //background: -webkit-gradient(left top, left bottom, color-stop(0%, rgba(164,179,87,1)), color-stop(57%, rgba(164,179,86,1)), color-stop(62%, rgba(164,179,86,1)), color-stop(72%, rgba(164,179,86,1)), color-stop(100%, rgba(117,137,12,1)));
+        //background: -webkit-linear-gradient(top, rgba(164,179,87,1) 0%, rgba(164,179,86,1) 57%, rgba(164,179,86,1) 62%, rgba(164,179,86,1) 72%, rgba(117,137,12,1) 100%);
+        //background: -o-linear-gradient(top, rgba(164,179,87,1) 0%, rgba(164,179,86,1) 57%, rgba(164,179,86,1) 62%, rgba(164,179,86,1) 72%, rgba(117,137,12,1) 100%);
+        //background: -ms-linear-gradient(top, rgba(164,179,87,1) 0%, rgba(164,179,86,1) 57%, rgba(164,179,86,1) 62%, rgba(164,179,86,1) 72%, rgba(117,137,12,1) 100%);
+        //background: linear-gradient(to bottom, rgba(164,179,87,1) 0%, rgba(164,179,86,1) 57%, rgba(164,179,86,1) 62%, rgba(164,179,86,1) 72%, rgba(117,137,12,1) 100%);
+        //filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a4b357', endColorstr='#75890c', GradientType=0 );
+        $("#socketConect").attr('style', 'background: rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(164, 179, 87, 1) 0%, rgba(164, 179, 86, 1) 57%, rgba(164, 179, 86, 1) 62%, rgba(164, 179, 86, 1) 72%, rgba(117, 137, 12, 1) 100%) repeat scroll 0 0 !important;');
         $("#socketConectRealtime").text('Stop Real Time');
     }else{
         socket.disconnect();
         //sockStatus = 0;
         console.log ("Disconect");
-        $("#socketConect").css({'background-image': 'linear-gradient(to bottom,#f5f5f5 0%,#e8e8e8 100%)'});
+        $("#socketConect").attr('style', 'background: rgba(0, 0, 0, 0) linear-gradient(background: linear-gradient(top, rgba(164,179,86,1) 0%, rgba(164,179,86,1) 1%, rgba(164,179,86,1) 20%, rgba(164,179,86,1) 29%, rgba(164,179,86,1) 63%, rgba(116,137,14,1) 99%, rgba(115,136,12,1) 100%);, rgba(76, 76, 76, 1) 0%, rgba(89, 89, 89, 1) 12%, rgba(102, 102, 102, 1) 25%, rgba(71, 71, 71, 1) 39%, rgba(44, 44, 44, 1) 46%, rgba(0, 0, 0, 1) 51%, rgba(44, 44, 44, 1) 55%, rgba(17, 17, 17, 1) 60%, rgba(43, 43, 43, 1) 76%, rgba(28, 28, 28, 1) 91%, rgba(19, 19, 19, 1) 100%) repeat scroll 0 0 !important;'); 
+        
+        //$("#div.panel-heading").css('background', '#fff', 'important');
         $("#socketConectRealtime").text('Start Real Time');
     }
 </script>
