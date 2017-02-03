882411fb6549cdf3ba65ca7cbb586eaccb9f75e2
DimitarGanev <dimitar.ganev@egt-bg.com>
2017-02-03 16:07:05 +0200

    bingo history

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::post('session/ajaxCheck','AuthController@ajaxCheck')->middleware('auth');
 /**
  * AUTH MIDDLEWARE
  */
-//Route::group(['middleware' => ['web', 'resetLastActive']], function () {
+Route::group(['middleware' => ['web', 'resetLastActive']], function () {
 Route::group(array('middleware' => 'auth'), function(){
 
     /**
@@ -284,4 +284,5 @@ Route::group(array('middleware' => 'auth'), function(){
     Route::post('/ajax_SaveEditCart', 'Players\PlayersController@ajax_SaveEditCart');
     Route::post('/ajax_AddBankCreditCard', 'Players\PlayersController@ajax_AddBankCreditCard');
     Route::post('/ajax_TransactionsCard', 'Players\PlayersController@ajax_TransactionsCard');
+});
 });
